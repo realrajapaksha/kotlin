38fba5d24e25e931b1e690595f1c896f826b1466
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2021-06-06 14:56:28 +0300

    [FIR] Replace lists with Long in Modifier, refactor Modifier and derived classes

--- compiler/fir/raw-fir/light-tree2fir/src/org/jetbrains/kotlin/fir/lightTree/fir/modifier/TypeProjectionModifier.kt
+++ compiler/fir/raw-fir/light-tree2fir/src/org/jetbrains/kotlin/fir/lightTree/fir/modifier/TypeProjectionModifier.kt
@@ -5,29 +5,16 @@
 
 package org.jetbrains.kotlin.fir.lightTree.fir.modifier
 
-import com.intellij.lang.LighterASTNode
 import org.jetbrains.kotlin.fir.FirSourceElement
-import org.jetbrains.kotlin.fir.expressions.FirAnnotationCall
-import org.jetbrains.kotlin.fir.lightTree.fir.modifier.ModifierSets.VARIANCE_MODIFIER
 import org.jetbrains.kotlin.types.Variance
 
-class TypeProjectionModifier(
-    val source: FirSourceElement? = null,
-    private val varianceModifiers: MutableList<VarianceModifier> = mutableListOf()
-) {
-    val annotations: MutableList<FirAnnotationCall> = mutableListOf()
-
-    fun addModifier(modifier: LighterASTNode) {
-        val tokenType = modifier.tokenType
-        when {
-            VARIANCE_MODIFIER.contains(tokenType) -> this.varianceModifiers += VarianceModifier.valueOf(modifier.toString().uppercase())
-        }
-    }
+class TypeProjectionModifier(val source: FirSourceElement? = null, varianceModifiers: Long = ModifierFlag.NONE.value) :
+    Modifier(varianceModifiers) {
 
     fun getVariance(): Variance {
         return when {
-            varianceModifiers.contains(VarianceModifier.IN) -> Variance.IN_VARIANCE
-            varianceModifiers.contains(VarianceModifier.OUT) -> Variance.OUT_VARIANCE
+            hasFlag(ModifierFlag.VARIANCE_OUT) -> Variance.OUT_VARIANCE
+            hasFlag(ModifierFlag.VARIANCE_IN) -> Variance.IN_VARIANCE
             else -> Variance.INVARIANT
         }
     }
